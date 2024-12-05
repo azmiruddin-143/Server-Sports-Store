482cb112d6539bf7adc8acc7fd26d71a4d756386
rjazmirkhan <bangladeshfreelancerazmir@gmail.com>
2024-12-05 16:56:16 +0600

    update user form complete

--- index.js
+++ index.js
@@ -24,54 +24,88 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
-        
+
     //    Start //
     const database = client.db("productsDB");
     const sportsCollection = database.collection("sportsCollection");
 
-    app.post('/sports', async (req,res) =>{
-        const sportsbody = req.body
-        const result = await sportsCollection.insertOne(sportsbody);
-        res.send(result)
+    app.post('/sports', async (req, res) => {
+      const sportsbody = req.body
+      const result = await sportsCollection.insertOne(sportsbody);
+      res.send(result)
 
-     })
+    })
 
     app.get('/sportslimit', async (req, res) => {
-        const cursor = sportsCollection.find().limit(6);;
-        const result = await cursor.toArray()
-        res.send(result)
+      const cursor = sportsCollection.find().limit(6);;
+      const result = await cursor.toArray()
+      res.send(result)
     })
     app.get('/sportsall', async (req, res) => {
-        const cursor = sportsCollection.find()
-        const result = await cursor.toArray()
-        res.send(result)
+      const cursor = sportsCollection.find()
+      const result = await cursor.toArray()
+      res.send(result)
     })
 
-    
-     app.get("/sportsall/:id", async (req, res) => {
-        const id = req.params.id
-        const query = { _id: new ObjectId(id) };
-        const result = await sportsCollection.findOne(query);
-        res.send(result)
+
+    app.get("/sportsall/:id", async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) };
+      const result = await sportsCollection.findOne(query);
+      res.send(result)
     })
+
+
+
 
     app.get('/myequipment', async (req, res) => {
-        const email = req.query.email; 
-        if (!email) {
-          return res.status(400).send({ message: "Email is required" });
+      const email = req.query.email;
+      if (!email) {
+        return res.status(400).send({ message: "Email is required" });
+      }
+      const query = { email: email };
+      const result = await sportsCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.delete('/myequipment/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) };
+      const result = await sportsCollection.deleteOne(query);
+      res.send(result)
+    })
+
+
+    app.get("/myequipment/:id", async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) };
+      const result = await sportsCollection.findOne(query);
+      res.send(result)
+    })
+
+    app.put("/myequipment/:id", async (req, res) => {
+      const id = req.params.id
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const upBody = req.body
+      const updateDoc = {
+        $set: {
+           image: upBody.image,
+           productName: upBody.productName,
+           category: upBody.category,
+           price: upBody.price,
+           rating: upBody.rating,
+           selectedDate: upBody.selectedDate,
+           email: upBody.email,
+           userName: upBody.userName
+
         }
-        const query = { email: email }; 
-        const result = await sportsCollection.find(query).toArray();
-        res.send(result);
-      });
-      
-      app.delete('/myequipment/:id', async (req,res) => {
-        const id = req.params.id
-        const query = { _id: new ObjectId(id) };
-        const result = await sportsCollection.deleteOne(query);
-        res.send(result)
-      })
-    
+      }
+
+      const result = await sportsCollection.updateOne(filter, updateDoc, options);
+      res.send(result)
+    })
+
 
 
     // Send a ping to confirm a successful connection
@@ -86,10 +120,10 @@ run().catch(console.dir);
 
 
 
-app.get('/', (req,res) =>{
-    res.send("Hlw I'm Azmir Uddin")
+app.get('/', (req, res) => {
+  res.send("Hlw I'm Azmir Uddin")
 })
 
-app.listen(port, () =>{
-    console.log(`Server is running`,port);
+app.listen(port, () => {
+  console.log(`Server is running`, port);
 })
